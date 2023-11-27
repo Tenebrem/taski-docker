37122d12afd2732c733e904baf4e953a539c2cb5
Иванов Николай <velling@gmail.com>
2023-11-27 16:02:04 +0500

    FIX

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,12 @@
+"""Админ."""
 from django.contrib import admin
 
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
+    """Админ."""
+
     list_display = ('title', 'description', 'completed')
 
 
